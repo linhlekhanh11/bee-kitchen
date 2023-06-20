6d5c92e246e6edcbb64bc7559135587e6e9d8ca5
Linh K Le <lkl007@ucsd.edu>
2023-06-20 16:36:31 -0700

    add category-preview and cart-item styled component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,42 @@
-import "./cart-item.styles.scss";
+import { styled } from "styled-components";
 
 const CartItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
-    <div className="cart-item-container">
-      <img src={imageUrl} alt={`${name}`} />
-      <div className="item-details">
-        <span className="name">{name}</span>
+    <CartItemContainer>
+      <StyledImage src={imageUrl} alt={`${name}`} />
+      <StyledImageDetails>
+        <StyledName>{name}</StyledName>
         <span className="price">
           {quantity} x ${price}
         </span>
-      </div>
-    </div>
+      </StyledImageDetails>
+    </CartItemContainer>
   );
 };
 
 export default CartItem;
+
+const CartItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  height: 80px;
+  margin-bottom: 15px;
+`;
+
+const StyledImage = styled.img`
+  width: 33%;
+`;
+
+const StyledImageDetails = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 10px 20px;
+`;
+
+const StyledName = styled.span`
+  font-size: 16px;
+`;
--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,25 +1,44 @@
-import { Link } from "react-router-dom";
+import { styled } from "styled-components";
+import { NavLinks } from "../routes/navigation/navigation.styles";
 import ProductCard from "../product-card/product-card.component";
-import "./category-preview.styles.scss";
+// import "./category-preview.styles.scss";
 
 const CategoryPreview = ({ title, products }) => {
   return (
-    <div className="category-preview-container">
-      <Link className="nav-link" to={title}>
+    <CategoryPreviewContainer>
+      <NavLinks to={title}>
         <h2>
-          <span>{title.toUpperCase()}</span>
+          <StyledTitle>{title.toUpperCase()}</StyledTitle>
         </h2>
-      </Link>
-      '
-      <div className="preview">
+      </NavLinks>
+      <StyledPreview>
         {products
           .filter((_, indx) => indx < 4)
           .map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
-    </div>
+      </StyledPreview>
+    </CategoryPreviewContainer>
   );
 };
 
 export default CategoryPreview;
+
+const CategoryPreviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+`;
+
+const StyledPreview = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 20px;
+`;
+
+const StyledTitle = styled.span`
+  font-size: 28px;
+  margin-bottom: 25px;
+  cursor: pointer;
+`;
+
